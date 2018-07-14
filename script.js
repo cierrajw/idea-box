a61327aff6bc227b48257e354840c3aee5bd4b5a
Cierra Washington <cierrajwashi@gmail.com>
2018-07-14 15:41:26 -0600

    Add global variables and event listeners

---
+++ script.js
@@ -0,0 +1,36 @@
+// window.localStorage
+
+// localStorage.setItem();
+
+// localStorage.remoteItem();
+
+
+var titleInput = $('.js-title');
+var bodyInput = $('js-body');
+var submit = $('.js-submit');
+var search = $('.js-search').val();
+
+
+titleInput.on('keyup', enableSave);
+
+bodyInput.on('keyup', enableSave);
+
+submit.on('click', function(e){
+	e.preventDefault();
+
+	var titleData = titleInput.dataset.title;
+
+	return `This is the title: ${titleData}`;
+
+});
+
+
+function enableSave() {
+
+  var isDisabled = (!titleInput || !bodyInput);
+
+  submit.prop('disabled', isDisabled);
+  return "It works";
+
+}
+
