4fd62401cb78c6f879dbeb19761d19fd3cef803f
Cole Hart <cole@colehart.com>
2018-07-14 18:24:17 -0600

    Refactor per style guides

--- script.js
+++ script.js
@@ -4,24 +4,13 @@ var bodyInput = $('.js-body');
 var submit = $('.js-submit');
 var search = $('.js-search');
 var ideaList = $('.js-idea-container');
-var ideaCollection = $('js-idea').toArray();
 var qualities = [': swill', ': plausible', ': genius'];
 
 // Event Listeners
 titleInput.on('keyup', enableSave);
 bodyInput.on('keyup', enableSave);
 submit.on('click', checkInputs);
-ideaList.on('click', function(e){
-  if(e.target.className === 'delete-button'){
-    $(e.target).closest('article').remove();
-  }
-  else if(e.target.className === 'upvote-button'){
-    console.log('Upvote yayyyy');
-  }
-  else if(e.target.className === 'downvote-button'){
-    console.log('Downvote yayyyy');
-  }
-});
+ideaList.on('click', checkTarget);
 
 // Functions
 
@@ -31,19 +20,19 @@ function enableSave() {
   submit.prop('disabled', isDisabled);
 };
 
-function checkInputs(e) {
-  e.preventDefault();
-  var isEmpty = (!titleInput || !bodyInput);
-  if (isEmpty) {
+function checkInputs(event) {
+  event.preventDefault();
+  if (!titleInput.val() || !bodyInput.val()) {
     alert('Please enter a title and description for your idea.');
+    return;
   } else {
     makeCard();
   };
 };
 
-function clearInputs(){
-  titleInput.val("");
-  bodyInput.val("");
+function clearInputs() {
+  titleInput.val('');
+  bodyInput.val('');
 };
 
 function makeCard() {
@@ -58,21 +47,34 @@ function makeCard() {
 
 // Main Section
 function populateIdea(ideaTitle, ideaBody) {
+  var ideaCollection = $('js-idea').toArray();
   var index = ideaCollection.length;
-  var newArticle = `<article class="js-idea">
-          <div class="idea-title" id="${index}"">
-            <h2>${ideaTitle}</h2>
-            <button class="delete-button" aria-label="delete button"></button>
-          </div>
-          <p>${ideaBody}</p>
-          <div class="idea-vote">
-            <button class="upvote-button" aria-label="upvote button"></button>
-            <button class="downvote-button" aria-label="downvote button"></button>
-            <h4>quality<span class="idea-quality">: swill<span></h4>
-          </div>
-        </article>`;
+  var newArticle =
+    `<article class="js-idea">
+      <div class="idea-title" id="${index}"">
+        <h2>${ideaTitle}</h2>
+        <button class="delete-button" aria-label="delete button"></button>
+      </div>
+      <p>${ideaBody}</p>
+      <div class="idea-vote">
+        <button class="upvote-button" aria-label="upvote button"></button>
+        <button class="downvote-button" aria-label="downvote button"></button>
+        <h4>quality<span class="idea-quality">: swill<span></h4>
+      </div>
+    </article>`;
+
   ideaCollection.push(newArticle);
   ideaList.prepend(newArticle);
+};
+
+function checkTarget(event) {
+  if (event.target.className === 'delete-button') {
+    $(event.target).closest('article').remove();
+  } else if (event.target.className === 'upvote-button') {
+    console.log('Upvote yayyyy');
+  } else if (event.target.className === 'downvote-button') {
+    console.log('Downvote yayyyy');
+  };
 };
 
 // window.localStorage
